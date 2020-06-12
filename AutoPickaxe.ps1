e170397c804d96fe1ca2f8545d0d4f90aa700da0
nyanshiba <31783332+nyanshiba@users.noreply.github.com>
2020-06-12 16:19:21 +0900

    マクロ中にMinecraftウィンドウを限りなく小さくしてディスプレイを他の用途で使えるようにする機能を追加
    右或いは左、または両方のクリックを選択できるようにし、採掘以外の用途でも使えるようになった

--- AutoPickaxe.ps1
+++ AutoPickaxe.ps1
@@ -1,12 +1,28 @@
-#190703
+#ユーザ設定
+$Settings =
+@{
+    #右押下
+    RightButtonDown = $True
+    #左押下
+    LeftButtonDown = $True
+    #ウィンドウを小さくするか
+    SmallWindow = $True
+}
 
 Write-Host "using .NET Class"
 Add-Type @"
 using System;
 using System.Runtime.InteropServices;
-public class Win32API {
-[DllImport("user32.dll", CharSet = CharSet.Auto)]
-public static extern IntPtr PostMessage(int hWnd, UInt32 Msg, int wParam, int lParam);
+
+public class SWP {
+[DllImport("user32.dll")] 
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+}
+
+public class PMSG {
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern IntPtr PostMessage(int hWnd, UInt32 Msg, int wParam, int lParam);
 }
 "@
 
@@ -28,17 +44,40 @@ if ($hwnd.Count -eq 0)
     }
 }
 
+#HWND_TOPMOST, SWP_NOMOVE
+#[SWP]::SetWindowPos($hwnd.MainWindowHandle, -1, 0, 0, 0, 0, 0x0002)
+
+#SWP_NOSIZE
+if ($Settings.SmallWindow)
+{
+    [SWP]::SetWindowPos($hwnd.MainWindowHandle, 0, 0, 0, 0, 0, 0)
+}
+
 Write-Host "Starting after 10 sec..."
+Write-Host "Alt-Tab -> Minecraft, Press 'Esc'."
 Start-Sleep -Seconds 10
 
 #WM_RBUTTONDOWN
-[Win32API]::PostMessage($hwnd.MainWindowHandle, 0x0204, 1, 0)
-Start-Sleep -MilliSeconds 100
+if ($Settings.RightButtonDown)
+{
+    [PMSG]::PostMessage($hwnd.MainWindowHandle, 0x0204, 1, 0)
+    Start-Sleep -MilliSeconds 100
+}
 #WM_LBUTTONDOWN
-[Win32API]::PostMessage($hwnd.MainWindowHandle, 0x0201, 1, 0)
+if ($Settings.LeftButtonDown)
+{
+    [PMSG]::PostMessage($hwnd.MainWindowHandle, 0x0201, 1, 0)
+}
+
 Read-Host "Processing...[Enter: Stop]"
 
 #WM_RBUTTONUP
-[Win32API]::PostMessage($hwnd.MainWindowHandle, 0x0205, 0, 0)
+if ($Settings.RightButtonDown)
+{
+    [PMSG]::PostMessage($hwnd.MainWindowHandle, 0x0205, 0, 0)
+}
 #WM_LBUTTONUP
-[Win32API]::PostMessage($hwnd.MainWindowHandle, 0x0202, 0, 0)
+if ($Settings.LeftButtonDown)
+{
+    [PMSG]::PostMessage($hwnd.MainWindowHandle, 0x0202, 0, 0)
+}
